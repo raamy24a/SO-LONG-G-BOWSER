71e295b6d05231944457702fbcaf9c2dc2b7bcda
Raamy Dib <radib@student.s19.be>
2025-05-26 11:12:53 +0200

    a

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,22 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:00:54 by radib             #+#    #+#             */
-/*   Updated: 2025/05/25 19:13:11 by radib            ###   ########.fr       */
+/*   Updated: 2025/05/25 21:16:17 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/mlx.h"
 #include "../so_long.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int handle_key(int keycode, void *param)
+{
+	if (keycode == 65307) // ESC key
+		exit(0);
+	(void)param;
+	return (0);
+}
 
 int	main(void)
 {
@@ -25,6 +35,16 @@ int	main(void)
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 1000, 600, "Fortnite");
 	render(mlx_ptr, win_ptr, x, y);
+	mlx_key_hook(win_ptr, handle_key, NULL);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
+// ESC	65307
+// W	119
+// A	97
+// S	115
+// D	100
+// UP	65362
+// DOWN	65364
+// LEFT	65361
+// RIGHT	65363
