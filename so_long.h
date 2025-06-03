4c104cd3819f26dadc1e3cb92f55f34ca3a3f8a7
raamydib@gmail.com <radib@fu-r6-p1.s19.be>
2025-06-03 16:56:55 +0200

    j'ai bien fais de parler autour de moi du projet ou sinon jore galerer sans structure lol

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:03:09 by radib             #+#    #+#             */
-/*   Updated: 2025/06/03 14:10:37 by radib            ###   ########.fr       */
+/*   Updated: 2025/06/03 16:17:45 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,15 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct s_data
+typedef struct map_data
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+	char	**map;
+	int		nbr_ligns;
+	int		nbr_cols;
+	int		c;
+	int		p;
+	int		e;
+}				t_map;
 
 void	render(void *mlx_ptr, void *win_ptr, int x, int y);
 char	*get_next_line(int fd);
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:59:19 by radib             #+#    #+#             */
-/*   Updated: 2025/06/03 15:12:47 by radib            ###   ########.fr       */
+/*   Updated: 2025/06/03 16:56:24 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,31 +61,24 @@ int	is_wall(char **map, int linecount)
 {
 	int	i;
 	int	j;
-	int	k;
 
 	i = 0;
 	j = 0;
-	if (is_wall_up_and_down(map, linecount))
+	if (!is_wall_up_and_down(map, linecount))
 		return (0);
 	while (i < linecount)
 	{
 		j = 0;
 		if (i != 0 && i != linecount - 1)
 		{
-			while (map[i][j] && map[i][j] == '1')
-				j++;
-			if (map[i][j] && map[i][j] != '\n')
-				return (0);
-			if (k == 0)
-				k = j;
-			if (k != j)
+			if (map[i][0] != '1')
 				return (0);
 		}
 		i++;
 	}
 }
 
-int	is_good(char **map, int linecount, int *i)
+int	is_good(char **map, int linecount, int *i, t_map *mapdata)
 {
 	while (i[0] && i[0] < linecount)
 	{
@@ -95,18 +88,18 @@ int	is_good(char **map, int linecount, int *i)
 			{
 			}
 			if (map[0][1] == 'E')
-				i[2]++;
+				mapdata->e++;
 			if (map[0][1] == 'P')
-				i[3]++;
+				mapdata->p++;
 			if (map[0][1] == 'C')
-				i[4]++;
+				mapdata->c++;
 			else
 				return (0);
 			i[1]++;
 		}
 		i[0]++;
 	}
-	if (i[2] == 1 && i[3] == 1 && i[4] >= 1)
+	if (mapdata->e == 1 && mapdata->p == 1 && mapdata->c >= 1)
 		return (1);
 	else
 		return (0);
@@ -117,6 +110,7 @@ int	parsing(void)
 	int		lcount;
 	char	**map;
 	int		fd;
+	t_map	*mapdata;
 
 	fd = open ("maps/defaultmap.ber", O_RDONLY);
 	lcount = 0;
@@ -124,7 +118,7 @@ int	parsing(void)
 		lcount++;
 	map = 0;
 	map = mapping_map(map, lcount);
-	if (!is_wall(map, lcount) && !is_good(map, lcount, (int []){0, 0, 0, 0, 0}))
+	if (!is_wall(map, lcount) && !is_good(map, lcount, (int []){0, 0}, mapdata))
 		return (0);
 	else
 		return (1);
